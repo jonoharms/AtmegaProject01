f0dab0171113fc1f5a14a2980e9475be2872bbc6
jonoharms <jonoharms@gmail.com>
2014-08-04 18:07:38 +1000

    initial commit

---
+++ GccApplication1/GccApplication1.c
@@ -0,0 +1,17 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 4/08/2014 6:03:41 PM
+ *  Author: Jonathon
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
