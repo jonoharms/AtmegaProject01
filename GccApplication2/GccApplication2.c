a3ab0736ae8e8fadebba970efcf63af45b783aec
jonoharms <jonoharms@gmail.com>
2014-08-04 18:27:30 +1000

    added second project

---
+++ GccApplication2/GccApplication2.c
@@ -0,0 +1,17 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 4/08/2014 6:26:55 PM
+ *  Author: Jonathon
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
